3ee5e2185a0024e6cf0e295148d7d2b8c21934d9
Piotr Szymaszek <piotr.szymaszek@fsi.pl>
2019-11-05 13:54:18 +0100

    Basic Symfony project setup for tests

---
+++ tests/_support/project/src/Kernel.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FSi\Tests\App;
+
+use FSi\Component\Files\Integration\Symfony\FilesBundle;
+use FSi\Tests\App\Controller\IndexController;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+use function sprintf;
+
+final class Kernel extends HttpKernel\Kernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new TwigBundle(),
+            new FilesBundle()
+        ];
+    }
+
+    public function getCacheDir()
+    {
+        return sprintf('%s/../var/cache/%s', __DIR__, $this->getEnvironment());
+    }
+
+    public function getLogDir()
+    {
+        return sprintf('%s/../var/log', __DIR__);
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $container->loadFromExtension('framework', [
+            'secret' => 'fsi_component_files_secret'
+        ]);
+
+        $container->loadFromExtension('twig', [
+            'paths' => [sprintf('%s/../templates', __DIR__)]
+        ]);
+
+        $controllerDefinition = $container->register(IndexController::class);
+        $controllerDefinition->setAutowired(true);
+        $controllerDefinition->setPublic(true);
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->add('/', IndexController::class);
+    }
+}
